493bddd3700120b9bb03b15c26b23e4a9818905d
Roman <roman985475612@gmail.com>
2021-07-01 09:43:04 +0300

    Initial commit

---
+++ gulp.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    SRC_PATH: 'src',
+    DIST_PATH: 'build',
+    STYLES_LIBS: [
+    ],
+    JS_LIBS: [
+    ],
+}
---
+++ gulpfile.js
@@ -0,0 +1,136 @@
+const { src, dest, task, series, watch, parallel } = require('gulp')
+const { DIST_PATH, SRC_PATH, STYLES_LIBS, JS_LIBS } = require('./gulp.config')
+const rm = require('gulp-rm')
+const sass = require('gulp-sass')
+const concat = require('gulp-concat')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
+const sassGlob = require('gulp-sass-glob')
+const autoprefixer = require('gulp-autoprefixer')
+const gcmq = require('gulp-group-css-media-queries')
+const cleanCss = require('gulp-clean-css')
+const sourcemaps = require('gulp-sourcemaps')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const svgo = require('gulp-svgo')
+const svgSprite = require('gulp-svg-sprite')
+const gulpIf = require('gulp-if')
+const webp = require('gulp-webp')
+const imagemin = require('gulp-imagemin')
+const env = process.env.NODE_ENV
+
+sass.compiler = require('node-sass')
+
+task('clean', () => {
+    return src([`${DIST_PATH}/**/*`, `!${DIST_PATH}/images/**/*`], {read: false})
+        .pipe(rm())
+})
+
+task('copy:html', () => {
+    return src(`${SRC_PATH}/*.html`)
+        .pipe(dest(`${DIST_PATH}`))
+        .pipe(reload({stream: true}))
+})
+
+task('copy:fonts', () => {
+    return src(`${SRC_PATH}/fonts/**/*`)
+        .pipe(dest(`${DIST_PATH}/fonts`))
+})
+
+task('copy:icons', () => {
+    return src(`${SRC_PATH}/icons/**/*`)
+        .pipe(dest(`${DIST_PATH}/icons`))
+})
+
+task('scss', () => {
+    return src([...STYLES_LIBS, `${SRC_PATH}/scss/style.scss`])
+        .pipe(gulpIf(env === 'dev', sourcemaps.init()))
+        .pipe(concat('style.min.scss'))
+        .pipe(sassGlob())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulpIf(env === 'prod', autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: true
+        })))
+        .pipe(gulpIf(env === 'prod', gcmq()))
+        .pipe(gulpIf(env === 'prod', cleanCss()))
+        .pipe(gulpIf(env === 'dev', sourcemaps.write()))
+        .pipe(dest(`${DIST_PATH}/css`))
+        .pipe(reload({stream: true}))
+})
+
+task('js', () => {
+    return src([...JS_LIBS, `${SRC_PATH}/js/*.js`])
+        .pipe(gulpIf(env === 'dev', sourcemaps.init()))
+        .pipe(concat('main.min.js', {newLine: ';'}))
+        .pipe(gulpIf(env === 'prod', babel({
+            presets: ["@babel/env"]
+        })))
+        .pipe(gulpIf(env === 'prod', uglify()))
+        .pipe(gulpIf(env === 'dev', sourcemaps.write()))
+        .pipe(dest(`${DIST_PATH}/js`))
+        .pipe(reload({stream: true}))
+})
+
+task('img:webp', () => {
+    return src(`${SRC_PATH}/img/**/*.{png,jpg,jpeg}`)
+        .pipe(webp())
+        .pipe(dest(`${DIST_PATH}/img`))
+})
+
+task('img', () => {
+    return src(`${SRC_PATH}/img/**/*.{png,jpg,jpeg}`)
+        .pipe(imagemin())
+        .pipe(dest(`${DIST_PATH}/img`))
+})
+
+task('icons', () => {
+    return src(`${SRC_PATH}/icons/*.svg`)
+        .pipe(svgo({
+            plugins: [
+                {
+                    removeAttrs: {
+                        attrs: "(fill|stroke|style|width|data.*)"
+                    }
+                }
+            ]
+        }))
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    sprite: "../sprite.svg"
+                }
+            }
+        }))
+        .pipe(dest(`${DIST_PATH}/img/i`))
+})
+
+task('server', () => {
+    browserSync.init({
+        server: {
+            baseDir: `./${DIST_PATH}`
+        },
+        open: false
+    })
+})
+
+task('watch', () => {
+    watch(`./${SRC_PATH}/scss/**/*.scss`, series('scss'))
+    watch(`./${SRC_PATH}/js/**/*.js`, series('js'))
+    watch(`./${SRC_PATH}/*.html`, series('copy:html'))
+})
+
+task('default', 
+    series(
+        'clean', 
+        parallel('copy:html', 'scss', 'js', 'copy:fonts', 'copy:icons'), 
+        parallel('watch', 'server')
+    )
+)
+
+task('build', 
+    series(
+        'clean', 
+        parallel('copy:html', 'scss', 'js', 'img', 'img:webp', 'copy:icons', 'copy:fonts')
+    ) 
+)
